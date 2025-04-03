a819f78c9e99162373d741ed343a82e4b61fb5c5
ArginP <n7toman@gmail.com>
2025-04-03 20:23:26 +0300

    sandbox section 1 & 2 finish {4:33:00}

--- src/components/TravelMain.jsx
+++ src/components/TravelMain.jsx
@@ -6,12 +6,7 @@ export default function TravelMain() {
     const travelEntry = travelData.map((entry) => {
         return <TravelEntry
             key={entry.id}
-            img={entry.img}
-            country={entry.country}
-            googleMapsLink={entry.googleMapsLink}
-            heading={entry.heading}
-            dates={entry.dates}
-            text={entry.text}
+            {...entry}
         />
     });
 
